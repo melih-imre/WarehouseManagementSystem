46070580b8e91a29f0ff40b20c6e7d4a32838ac3
Anandhukrishnan <w0852083@myscc.ca>
2024-11-05 23:16:58 -0800

    #2 Creates a database instance in the HelloApplication Class.

--- src/main/java/org/example/warehousemanagementsystem/HelloApplication.java
+++ src/main/java/org/example/warehousemanagementsystem/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.warehousemanagementsystem.database.Database;
 
 import java.io.IOException;
 
@@ -12,6 +13,10 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+
+        Database db = Database.getInstance();
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
