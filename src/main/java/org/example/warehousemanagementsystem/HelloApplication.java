bebffe10a13238d974ad956ebe4877327890c063
Anandhukrishnan <w0852083@myscc.ca>
2024-11-09 12:36:00 -0800

    merged to database and development

--- src/main/java/org/example/warehousemanagementsystem/HelloApplication.java
+++ src/main/java/org/example/warehousemanagementsystem/HelloApplication.java
@@ -1,29 +1,23 @@
 package org.example.warehousemanagementsystem;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.example.warehousemanagementsystem.database.Database;
-
 import java.io.IOException;
+
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-
-
         Database db = Database.getInstance();
-
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
 
     }
-
-
     public static void main(String[] args) {
         launch();
     }
