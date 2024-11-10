41f4e768773780fe1a648cb95fe2eee061bcfdf3
Melih Imre <melihimre@myscc.ca>
2024-11-09 23:09:52 -0500

    Set the screen size and rename the Main.java

--- src/main/java/org/example/warehousemanagementsystem/Main.java
+++ src/main/java/org/example/warehousemanagementsystem/Main.java
@@ -2,18 +2,19 @@ package org.example.warehousemanagementsystem;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.example.warehousemanagementsystem.database.Database;
 import java.io.IOException;
 
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 1024, 768);
         Database db = Database.getInstance();
-        stage.setTitle("Hello!");
+        stage.setTitle("Warehouse Database Management System");
         stage.setScene(scene);
         stage.show();
 
