8c958d63f51369bc7a8d6045316ea0415923b103
Anandhukrishnan <w0852083@myscc.ca>
2024-11-10 23:36:57 -0800

    # fixed the issue 3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.warehousemanagementsystem {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
     opens org.example.warehousemanagementsystem to javafx.fxml;
--- src/main/java/org/example/warehousemanagementsystem/Main.java
+++ src/main/java/org/example/warehousemanagementsystem/Main.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import org.example.warehousemanagementsystem.Menubar.Bar;
 import org.example.warehousemanagementsystem.database.Database;
 
 import java.io.IOException;
@@ -14,7 +15,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         BorderPane root = new BorderPane();
+        Bar menu = new Bar();
+        root.setTop(menu);
         Scene scene = new Scene(root, 1024, 768);
+
         Database db = Database.getInstance();
         stage.setTitle("Warehouse Database Management System");
         stage.setScene(scene);
