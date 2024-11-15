4199856c9766463378dbd4a1df5b945577c953c3
Numan Karateke <w0848363@myscc.ca>
2024-11-15 12:26:40 -0500

    On the login page, I made a few changes and created a new branch called 'manager.' The 'manager' branch now stores information in dbconfig.txt  and edited the database.

--- src/main/java/org/example/warehousemanagementsystem/database/Database.java
+++ src/main/java/org/example/warehousemanagementsystem/database/Database.java
@@ -1,39 +1,32 @@
 package org.example.warehousemanagementsystem.database;
 
-import java.sql.*;
-import static org.example.warehousemanagementsystem.database.Const.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Database {
     private static Database instance;
     private Connection connection;
 
     private Database() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager
-                    .getConnection("jdbc:mysql://localhost/" + DB_NAME +
-                            "?serverTimeZone = UTC",
-                            DB_USER,
-                            DB_PASS
-                            );
-            System.out.println("created connection");
-            //create table
-
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
     }
+
     public static Database getInstance() {
         if (instance == null) {
             instance = new Database();
-
         }
         return instance;
     }
 
+    public void connect(String serverLocation, String dbName, String username, String password) throws Exception {
+        if (connection == null || connection.isClosed()) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://" + serverLocation + "/" + dbName + "?serverTimeZone=UTC";
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Database connection established.");
+        }
+    }
 
-    //table
-
-    public Connection getConnection() {return connection;}
+    public Connection getConnection() {
+        return connection;
+    }
 }
