adaba373087df795a0c8fe663cd4d10df00036b6
SaiKrishna Suresh <w0845777@myscc.ca>
2024-11-26 22:40:37 -0500

    Display Location

--- src/main/java/org/example/warehousemanagementsystem/tables/test/displayItems/DisplayLocation.java
+++ src/main/java/org/example/warehousemanagementsystem/tables/test/displayItems/DisplayLocation.java
@@ -1,8 +1,37 @@
 package org.example.warehousemanagementsystem.tables.test.displayItems;
 
 public class DisplayLocation {
+    private int aisleId;
+    private int shelfId;
+    private int capacity;
 
+    public DisplayLocation(int aisleId, int shelfId, int capacity) {
+        this.aisleId = aisleId;
+        this.shelfId = shelfId;
+        this.capacity = capacity;
+    }
 
+    public int getAisleId() {
+        return aisleId;
+    }
 
+    public void setAisleId(int aisleId) {
+        this.aisleId = aisleId;
+    }
 
+    public int getShelfId() {
+        return shelfId;
+    }
+
+    public void setShelfId(int shelfId) {
+        this.shelfId = shelfId;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
